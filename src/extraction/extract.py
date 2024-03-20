a412e3f0352c60034cab57b2fb873a7c9e1da331
JorgeVitor30 <jorgevitorlima8@gmail.com>
2024-03-20 00:18:46 -0300

    adding columns

--- src/extraction/extract.py
+++ src/extraction/extract.py
@@ -59,7 +59,7 @@ class Extract:
         dict_company['Site'] = site
                  
         
-      elif 'balanco patrimonial' in text:
+      if 'balanco patrimonial' in text:
         # DATAS DE ATUALIZAÇÃO
         
         content, error = rgx.match([r'balanço patrimonial - consolidado\s*([\d/]+)\s*([\d/]+)', r'consolidado\s*([\d/]+)\s*([\d/]+)'], 2)
@@ -113,7 +113,6 @@ class Extract:
         # RECEITA DE VENDA
         content, error = rgx.match([r'receita de venda\s*(.*?)\n', r'venda\s*(.*?)\n'], 1)
         receita_venda = error if error else content[0]
-        print(receita_venda)
         
         list_receita = receita_venda.split(' ')
         
@@ -123,7 +122,65 @@ class Extract:
         if receita_venda_atualizado == 'Não encontrado':
           receita_venda_atualizado, receita_venda_ultimo = split_string(receita_venda)
           
-  
         dict_company['ReceitaVendaAtual'] = receita_venda_atualizado
         dict_company['ReceitaVendaUltimo'] = receita_venda_ultimo
+      
+      
+        # RESULTADO BRUTO
+        content, error = rgx.match([r'resultado bruto\s*(.*?)\n'], 1)
+        resultado_bruto = error if error else content[0]
+        if 'financeira' in resultado_bruto:
+          resultado_bruto = resultado_bruto[resultado_bruto.find('financeira') + 10:].strip()
+          
+        list_resultado = resultado_bruto.split(' ')
         
+        resultado_bruto_atualizado = list_resultado[0] if len(list_resultado) > 1 else 'Não encontrado'
+        resultado_bruto_ultimo = list_resultado[1] if len(list_resultado) > 1 else 'Não encontrado'
+  
+        dict_company['ResultadoBrutoAtual'] = resultado_bruto_atualizado
+        dict_company['ResultadoBrutoUltimo'] = resultado_bruto_ultimo
+        
+        
+        # LUCRO (PREJUIZO) DO PERIODO
+        content, error = rgx.match([r'lucro \(prejuizo\) do periodo\s*(.*?)\n'], 1)
+        lucro_periodo = error if error else content[0]
+        list_lucro = lucro_periodo.split(' ')
+        
+        lucro_periodo_atualizado = list_lucro[0] if len(list_lucro) > 1 else 'Não encontrado'
+        lucro_periodo_ultimo = list_lucro[1] if len(list_lucro) > 1 else 'Não encontrado'
+        
+        dict_company['LucroPrejPeriodoAtual'] = lucro_periodo_atualizado
+        dict_company['LucroPrejPeriodoUltimo'] = lucro_periodo_ultimo
+        
+      if 'resultado de equivalencia patrimonial' in text:
+        # RESULTADO DE EQUIVALÊNCIA PATRIMONIAL
+        content, error = rgx.match([r'resultado de equivalencia patrimonial\s*(.*?)\n'], 1)
+        result_equivalencia = error if error else content[0]
+        
+        list_result_equivalencia = result_equivalencia.split(' ')
+        
+        if len(list_result_equivalencia) == 1:
+          result_equivalencia_atualizado, result_equivalencia_ultimo = list_result_equivalencia[0].split(')(')
+        else:
+          result_equivalencia_atualizado = list_result_equivalencia[0] if len(list_result_equivalencia) >= 1 else 'Não encontrado'
+          result_equivalencia_ultimo = list_result_equivalencia[1] if len(list_result_equivalencia) > 1 else 'Não encontrado'
+        
+        dict_company['ResultEquivalenciaAtual'] = result_equivalencia_atualizado
+        dict_company['ResultEquivalenciaUltimo'] = result_equivalencia_ultimo
+      
+      
+      if 'demonstracao do resultado' in text:
+        # RESULTADO FINANCEIRO
+        content, error = rgx.match([r'resultado financeiro\s*(.*?)\n'], 1)
+        result_financeiro = error if error else content[0]
+        
+        list_result_financeiro = result_financeiro.split(' ')
+        
+        if len(list_result_financeiro) == 1:
+          result_financeiro_atualizado, result_financeiro_ultimo = list_result_financeiro[0].split(')(')
+        else:
+          result_financeiro_atualizado = list_result_financeiro[0] if len(list_result_financeiro) >= 1 else 'Não encontrado'
+          result_financeiro_ultimo = list_result_financeiro[1] if len(list_result_financeiro) > 1 else 'Não encontrado'
+        
+        dict_company['ResultFinanceiroAtual'] = result_financeiro_atualizado
+        dict_company['ResultFinanceiroUltimo'] = result_financeiro_ultimo
